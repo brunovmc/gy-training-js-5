c66a99fc4201b243eb36cfc246010a90b6cd6c2d
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-03 09:43:34 -0300

    feat(index.js): created two methods for filtering

--- index.js
+++ index.js
@@ -5,31 +5,53 @@ const csvParser = require('csv-parser');
 const app = express();
 const port = 3000;
 
-const frequencyThreshold = 500000;
+const frequencyThresholdNum = 500000;
 const frequencyThresholdString = '500000';
 
-app.get('/read-csv', (req, res) => {
-  const method = req.query.method || 'num';
-
+function filterByNumericFrequency() {
   const nomes = [];
 
   fs.createReadStream('ibge-fem-10000 (3).csv')
     .pipe(csvParser({ separator: ',' }))
     .on('data', (row) => {
       const freq = parseInt(row.freq);
-      const freqString = parseInt(row.freq);
-
-      if (method === 'num' && !isNaN(freq) && freq >= frequencyThreshold) {
-        nomes.push(row.nome);
-        console.log(row.nome);
-      } else if (method === 'string' && !isNaN(freq) && freqString >= frequencyThresholdString) {
+      if (!isNaN(freq) && freq >= frequencyThresholdNum) {
         nomes.push(row.nome);
         console.log(row.nome);
       }
     })
     .on('end', () => {
-      res.send(nomes);
+      console.log("Filter by numeric frequency completed.");
     });
+}
+
+function filterByStringFrequency() {
+  const nomes = [];
+
+  fs.createReadStream('ibge-fem-10000 (3).csv')
+    .pipe(csvParser({ separator: ',' }))
+    .on('data', (row) => {
+      const freqString = parseInt(row.freq);
+      if (freqString >= frequencyThresholdString) {
+        nomes.push(row.nome);
+        console.log(row.nome);
+      }
+    })
+    .on('end', () => {
+      console.log("Filter by string frequency completed.");
+    });
+}
+
+app.get('/read-csv', (req, res) => {
+  const method = req.query.method || 'num';
+
+  if (method === 'num') {
+    filterByNumericFrequency();
+  } else if (method === 'string') {
+    filterByStringFrequency();
+  }
+
+  res.send('Filtering completed.');
 });
 
 app.listen(port, () => {
